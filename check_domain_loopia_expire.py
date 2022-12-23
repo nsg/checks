fbafd65b6ab20e7e5941180d8e191842f7c91e80
Stefan Berggren <nsg@nsg.cc>
2022-12-23 16:48:51 +0100

    Add loopia and rdap expire checks

---
+++ check_domain_loopia_expire.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+#
+# A simple check that uses the loopia API to figure out when domains expire
+# Create an API user, select advanced permissions and grant getDomain.
+#
+
+import os
+import sys
+import argparse
+from datetime import datetime
+from dateutil.parser import parse
+import json
+import xmlrpc.client
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--domain", type=str, help="Look up the specified domain", required=True)
+parser.add_argument("-u", "--user", type=str, help="Loopia API username", required=True)
+parser.add_argument("-p", "--password", type=str, help="Loopia API password", required=True)
+parser.add_argument("-w", "--warning", type=int, help="Warning level (days)", default=30)
+parser.add_argument("-c", "--critical", type=int, help="Critical level (days)", default=14)
+parser.add_argument("-C", "--cache_time", type=int, help="Cache time in seconds", default=21600)
+args = parser.parse_args()
+
+def get_domain_info():
+    cache_file = f"/tmp/loopia_domain_expire_cache_{args.domain}.json"
+    now_ts = datetime.now().timestamp()
+
+    if os.path.isfile(cache_file) and now_ts < os.stat(cache_file).st_mtime + args.cache_time:
+        cf = open(cache_file, "r")
+        data = json.load(cf)
+        data["cache"] = int((os.stat(cache_file).st_mtime + args.cache_time - now_ts) / 60)
+        return data
+
+    client = xmlrpc.client.ServerProxy(uri="https://api.loopia.se/RPCSERV", encoding="utf-8")
+    response = client.getDomain(args.user, args.password, args.domain)
+    cf = open(cache_file, "w")
+    cf.write(json.dumps(response))
+    response["cache"] = 0
+    return response
+
+response = get_domain_info()
+paid = response['paid']
+expiration_date = parse(response['expiration_date'])
+time_between = expiration_date - datetime.now()
+
+if paid:
+    print(f"Domain {args.domain} will expire in {time_between.days} days [cache: {response['cache']}]")
+else:
+    print(f"Domain {args.domain} is not paid, it will expire in {time_between.days} days [cache: {response['cache']}]")
+
+if time_between.days < args.warning:
+    sys.exit(2)
+elif time_between.days < args.critical:
+    sys.exit(1)
+else:
+    sys.exit(0)
---
+++ check_domain_rdap_expire.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+#
+# A simple check that looks up domain expire information via the RDAP API
+#
+
+import sys
+import os
+import argparse
+import json
+from dateutil.parser import parse
+from datetime import datetime, timezone
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--domain", type=str, help="Look up the specified domain", required=True)
+parser.add_argument("-p", "--provider", type=str, choices=["ascio"], help="RDAP Provider", required=True)
+parser.add_argument("-w", "--warning", type=int, help="Warning level (days)", default=14)
+parser.add_argument("-c", "--critical", type=int, help="Critical level (days)", default=7)
+parser.add_argument("-C", "--cache_time", type=int, help="Cache time in seconds", default=21600)
+args = parser.parse_args()
+
+# https://lookup.icann.org/en/lookup
+# https://deployment.rdap.org/
+PROVIDERS = {
+    "ascio": "https://rdap.ascio.com/domain/{domain}"
+}
+
+cache_file = f"/tmp/domain_expire_cache_{args.domain}.json"
+
+if os.path.isfile(cache_file) and datetime.now().timestamp() < os.stat(cache_file).st_mtime + args.cache_time:
+    cf = open(cache_file, "r")
+    rdap_data = json.load(cf)
+    rdap_data["cache"] = True
+else:
+    url = PROVIDERS[args.provider].format(domain=args.domain)
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        print("Got a non-200 responce from the RDAP provider")
+        print("Response:", resp.text)
+        sys.exit(3)
+
+    rdap_data = json.loads(resp.text)
+    cf = open(cache_file, "w")
+    cf.write(json.dumps(rdap_data))
+    rdap_data["cache"] = False
+
+for event in rdap_data.get("events", []):
+    if event.get("eventAction") == "expiration":
+        expiration_date = parse(event.get("eventDate"))
+        time_between = expiration_date - datetime.now(timezone.utc)
+        print(f"Domain {args.domain} will expire in {time_between.days} days [cache: {rdap_data['cache']}]")
+
+        if time_between.days < args.warning:
+            sys.exit(2)
+        elif time_between.days < args.critical:
+            sys.exit(1)
+        else:
+            sys.exit(0)
